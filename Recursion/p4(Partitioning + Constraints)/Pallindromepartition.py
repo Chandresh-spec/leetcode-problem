766ab4a07d27c91e2845cc641b5911c9e561d957
Chandresh-spec <chandumoger819@gmail.com>
2025-05-16 10:21:47 +0530

    adding questions

--- Recursion/p4(Partitioning + Constraints)/Pallindromepartition.py
+++ Recursion/p4(Partitioning + Constraints)/Pallindromepartition.py
@@ -1,3 +1,49 @@
+#  Partitioning
+# Solved
+# Medium
+# Topics
+# Companies
+# Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+
+ 
+
+# Example 1:
+
+# Input: s = "aab"
+# Output: [["a","a","b"],["aa","b"]]
+# Example 2:
+
+# Input: s = "a"
+# Output: [["a"]]
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 16
+# s contains only lowercase English letters.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def partition(s):
         ans=[]
         def helper(index,ds):
