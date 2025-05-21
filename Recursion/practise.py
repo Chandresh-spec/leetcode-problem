6f33bae101925d4942059d8327f3f8dc9809b79d
Chandresh-spec <chandumoger819@gmail.com>
2025-05-21 23:40:25 +0530

    combination sum

--- Recursion/practise.py
+++ Recursion/practise.py
@@ -1,19 +1,19 @@
-def subset(num):
-    ds=[]
-    def helper(index,count):
+# def subset(num):
+#     ds=[]
+#     def helper(index,count):
         
-        if index==len(num):
-            ds.append(count)
-            return 
+#         if index==len(num):
+#             ds.append(count)
+#             return 
         
-        helper(index+1,count+num[index])
-        helper(index+1,count)
+#         helper(index+1,count+num[index])
+#         helper(index+1,count)
 
-    helper(0,0)
-    return ds
+#     helper(0,0)
+#     return ds
 
-num=[5, 2, 1]
-print(subset(num))
+# num=[5, 2, 1]
+# print(subset(num))
 
 def combination_sum(candidates,target):
     ans=[]
@@ -31,6 +31,11 @@ def combination_sum(candidates,target):
     
     helper(0,[],0)
     return ans
+
+
+candidates = [2,3,6,7]
+target = 7
+
 
 
     
