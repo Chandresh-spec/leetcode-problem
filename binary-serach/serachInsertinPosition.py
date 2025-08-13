26d53947a059433bfbd84e9122d40b1cd3f6891e
Chandresh-spec <chandumoger819@gmail.com>
2025-08-13 08:38:01 +0530

    sloved the serach insertion position problem at leetcode

---
+++ binary-serach/serachInsertinPosition.py
@@ -0,0 +1,40 @@
+# 35. Search Insert Position
+# Solved
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [1,3,5,6], target = 5
+# Output: 2
+# Example 2:
+
+# Input: nums = [1,3,5,6], target = 2
+# Output: 1
+# Example 3:
+
+# Input: nums = [1,3,5,6], target = 7
+# Output: 4
+ 
+
+
+def searchInsert(nums,target):
+        ans=len(nums)
+        lb=0
+        ub=len(nums)-1
+        while lb <= ub:
+            mid=(ub+lb)//2
+            if nums[mid]>=target:
+                ans=mid
+                ub=mid-1
+            else:
+                lb=mid+1
+            
+        return ans
