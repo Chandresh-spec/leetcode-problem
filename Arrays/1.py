8f683a613dcbf80a0bcb5ed421bb6ea5ad928357
Chandresh-spec <chandumoger819@gmail.com>
2025-08-18 23:07:42 +0530

    sloved

--- Arrays/1.py
+++ Arrays/1.py
@@ -1,1 +1,14 @@
-print("helo world")
+def findlargest(nums):
+    n=0
+    for i in range(len(nums)):
+        if nums[i]>n:
+            n=nums[i]
+        
+    return n
+
+
+
+
+
+nums=[1,8,6,3,9,4]
+print(findlargest(nums))
