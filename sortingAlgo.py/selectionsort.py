00431972601321a5a9dccfac8dfe5e075f406377
Chandresh-spec <chandumoger819@gmail.com>
2025-08-20 08:23:39 +0530

    sloved selection sort

---
+++ sortingAlgo.py/selectionsort.py
@@ -0,0 +1,19 @@
+def selectionSort(arr):
+    for i in range(len(arr)):
+        mini=i
+        for j in range(i+1,len(arr)):
+
+            if  arr[j]<= arr[mini]:
+                mini=j
+
+        arr[mini],arr[i]=arr[i],arr[mini]
+    
+
+
+    return arr
+
+
+
+
+arr=[1,8,9,6,7,5,3,2]
+print(selectionSort(arr))
