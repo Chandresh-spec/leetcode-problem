df2e0da9b3b0acae1e1ba222b1bfb2fcf1158e03
Chandresh-spec <chandumoger819@gmail.com>
2025-08-20 08:41:49 +0530

    sloved bubblesot

---
+++ sortingAlgo.py/bubbleSort.py
@@ -0,0 +1,14 @@
+def BubbleSort(nums):
+    n=len(nums)
+    for i in range(n):
+        for j in range(n-i-1):
+            if nums[j]> nums[j+1]:
+                nums[j],nums[j+1]=nums[j+1],nums[j]
+            
+    
+    return nums
+
+
+
+arr=[1,8,9,6,7,5,3,2]
+print(BubbleSort(arr))
