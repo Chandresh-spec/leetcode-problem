0e38d10dbdbe8a7573cef9a4bb1470e552ab1b14
Chandresh-spec <chandumoger819@gmail.com>
2025-09-19 19:21:04 +0530

    basic operations like addition subtraction multiplication,division are done

--- Arrays/numpyprojects/matrixsCalculator.py
+++ Arrays/numpyprojects/matrixsCalculator.py
@@ -5,33 +5,52 @@ class MatrixCalculator:
         self.cols=cols
     
 
-
-    def add_matrix(self,a,b):
+    def print_matrix(self,op):
         print("Matrix A:")
         print(a)
         print("Matrix B:")
         print(b)
         print("_"*40)
-        add_a=np.array(a)
-        add_b=np.array(b)
 
-        print("Sum of Matrix is :")
-        print(add_a+add_b)
+        print(f" {op} of Matrix A and B is:")
 
     
+
+
+    def add_matrix(self,a,b):
+        self.print_matrix("Sum")
+        print(a+b)
+ 
+    
     def subtract_matrix(self,a,b):
-        print("Matrix A:")
-        print(a)
-        print("Matrix B:")
-        print(b)
-        print("-"*40)
+        self.print_matrix("Subtrack")
+        print(a-b)
+    
 
-        print("Substraction of Matrix A and B is :")
+    def Multiply_matrix(self,a,b):
+        self.print_matrix("Element-Wise Multiplication")
 
-        sub_a=np.array(a)
-        sub_b=np.array(b)
 
-        print(sub_a-sub_b)
+        print(a*b)
+    
+    def dot_product_matrix(self,a,b):
+           cal=np.dot(a,b)
+           print("Dot Product Multiplication   of Matrix A and B is :")  
+           print(cal)
+        
+    
+
+    def division_ops(self,a,b):
+         self.print_matrix("Element-Wise Division")
+
+         print(a/b)
+
+           
+
+
+
+
+
 
         
 
@@ -68,8 +87,9 @@ def take_output(first,row,col):
 
 
 print("WELCOME TO MATRIX CALCULATOR")
-print("1.ADDITION\n2.SUBTRACTION ")
-choice=int(input("CHOOSE A NUMBER [1,2]:  "))
+print("1.ADDITION\n2.SUBTRACTION\n3.MULTIPLICATION\n4.DIVISION")
+
+choice=int(input("CHOOSE A NUMBER [1,2,3,4]:  "))
 
 
 
@@ -84,6 +104,28 @@ elif choice == 2:
     b=take_output(False,row,col)
 
     obj.subtract_matrix(a,b)
+
+elif choice==3:
+    print("1.Element-wise Multiplication")
+    print("2.Matrix Multiplication (dot product)")
+    choice_n=int(input("Enter the a choice : "))
+
+    if choice_n==1:
+        a=take_output(True,row,col)
+        b=take_output(False,row,col)
+        obj.Multiply_matrix(a,b)
+    # elif choice_n==2:
+        # row_=int(input("Enter the number of rows for A"))
+    
+elif choice==4:
+     a=take_output(True,row,col)
+     b=take_output(False,row,col)
+     obj.division_ops(a,b)
+
+
+    
+        
+
 
 
 
