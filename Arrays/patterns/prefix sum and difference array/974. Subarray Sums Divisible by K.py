8ecc7ae876357febed9dad68f991d7db0b26751e
Chandresh-spec <chandumoger819@gmail.com>
2025-09-21 11:26:33 +0530

    sloved divisible by k problem

---
+++ Arrays/patterns/prefix sum and difference array/974. Subarray Sums Divisible by K.py
@@ -0,0 +1,54 @@
+# 974. Subarray Sums Divisible by K
+# Solved
+# Medium
+# Topics
+# premium lock icon
+# Companies
+# Given an integer array nums and an integer k, return the number of non-empty subarrays that have a sum divisible by k.
+
+# A subarray is a contiguous part of an array.
+
+ 
+
+# Example 1:
+
+# Input: nums = [4,5,0,-2,-3,1], k = 5
+# Output: 7
+# Explanation: There are 7 subarrays with a sum divisible by k = 5:
+# [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3], [-2, -3]
+# Example 2:
+
+# Input: nums = [5], k = 9
+# Output: 0
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 3 * 104
+# -104 <= nums[i] <= 104
+# 2 <= k <= 104
+
+
+
+
+
+class Solution:
+    def subarraysDivByK(nums,k) -> int:
+        hashmap={0:1}
+        sum_of=0
+        count=0
+        for i in range(len(nums)):
+            sum_of+=nums[i]
+
+            remainder=sum_of % k 
+
+            if remainder in hashmap:
+                count+= hashmap[remainder]
+            
+            hashmap[remainder]=hashmap.get(remainder,0)+1
+        
+
+        return count
+
+
+        
