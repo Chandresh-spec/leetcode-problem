d44aefada47dffda359efa6d0d0f9884381e3cb9
Chandresh-spec <chandumoger819@gmail.com>
2025-09-25 08:05:08 +0530

    optimal  for 3sum problem

--- Arrays/3sum.py
+++ Arrays/3sum.py
@@ -49,6 +49,61 @@ class Solution:
 
 
         return items
+    
+
+
+
+
+# optimal
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
+        nums.sort()
+        for i in range(len(nums)):
+            if i != 0 and nums[i]==nums[i-1]:
+                continue
+            
+
+            j=i+1
+            k=len(nums)-1
+
+
+            while j < k :
+                sum_of=nums[i]+nums[j]+nums[k]
+
+
+                if sum_of < 0:
+                    j+=1
+                elif sum_of > 0:
+                    k-=1
+                else:
+                    temp=[nums[i],nums[j],nums[k]]
+                    ans.append(temp[:])
+                    j+=1
+                    k-=1
+                
+                    while j < k and nums[j]==nums[j-1]:
+                       j+=1
+                
+                    while j < k and nums[k]==nums[k+1]:
+                      k-=1
+        return ans
+                    
+        
+
+
+    
+    
+
+        
+                        
+
+        
+
+
+
+
 
                     
         
