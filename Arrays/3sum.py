e8f31be4ccbcd82b5c8a76a1224fc46029cceae7
Chandresh-spec <chandumoger819@gmail.com>
2025-09-25 07:54:20 +0530

    better  approach for 3sum problem

--- Arrays/3sum.py
+++ Arrays/3sum.py
@@ -23,3 +23,47 @@ def sum3(nums):
 nums=[-1,0,1,2,-1,-4]
 
 print(sum3(nums))
+
+
+
+# better
+
+
+from typing import List
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans=set()
+        for i in range(len(nums)-1):
+            hashmap=set()
+            for j in range(i+1,len(nums)):
+                third= -(nums[i]+nums[j])
+                if third in hashmap:
+                        temp=[nums[i],nums[j],third]
+                        temp.sort()
+                        ans.add(tuple(temp))
+                
+                hashmap.add(nums[j])
+        
+        
+        items=[list(item) for item in ans]
+
+
+        return items
+
+                    
+        
+
+
+    
+    
+
+        
+                        
+
+        
+
+
+
+
+
+
