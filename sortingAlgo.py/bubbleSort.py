17410c578e34086186c9ef2a09fa8de030c8a158
Chandresh-spec <chandumoger819@gmail.com>
2025-10-14 23:29:33 +0530

    sloved insertion sort problem

--- sortingAlgo.py/bubbleSort.py
+++ sortingAlgo.py/bubbleSort.py
@@ -1,11 +1,11 @@
 def BubbleSort(nums):
+
     n=len(nums)
-    for i in range(n):
+    for i in range(len(nums)):
         for j in range(n-i-1):
-            if nums[j]> nums[j+1]:
+            if nums[j]>= nums[j+1]:
                 nums[j],nums[j+1]=nums[j+1],nums[j]
-            
-    
+        
     return nums
 
 
---
+++ sortingAlgo.py/insertion.py
@@ -0,0 +1,20 @@
+def insertion_sort(nums):
+    for i in range(len(nums)):
+        key=nums[i]
+        j=i-1
+
+        while j > 0 and key < nums[j]:
+            nums[j+1]=nums[j]
+            j-=1
+        
+        nums[j+1]=key
+
+
+    
+    return nums
+
+
+
+
+arr=[1,8,9,6,7,5,3,2]
+print(insertion_sort(arr))
