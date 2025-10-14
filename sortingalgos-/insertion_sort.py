dd008716c35a711cf46dd562586eb5af4b81c910
Chandresh-spec <chandumoger819@gmail.com>
2025-10-14 16:48:09 +0530

    insertion sort

--- sortingalgos-/insertion_sort.py
+++ sortingalgos-/insertion_sort.py
@@ -1,20 +1,43 @@
-def min_max(nums,low,high):
-    if low==high:
-        return nums[low],nums[low]
+# def min_max(nums,low,high):
+    # if low==high:
+        # return nums[low],nums[low]
+    # 
+    # elif high-low==1:
+        # return min(nums[low],nums[high]),max(nums[low],nums[high])
+    # 
+# 
+    # mid=(high+low)//2
+# 
+    # left_min,left_max=min_max(nums,low,mid)
+    # right_min,right_max=min_max(nums,mid+1,high)
+# 
+    # return min(left_min,right_min),max(left_max,right_max)
+# 
+# 
+# 
+# 
+# nums=[1,8,9,6,3,5,0]
+# print(min_max(nums,0,len(nums)-1))
+# 
+
+
+
+def insertion_sort(nums):
+    for i in range(1,len(nums)):
+        key=nums[i]
+        j=i-1
+
+        while j >=0 and key < nums[j]:
+            nums[j+1]=nums[j]
+            j-=1
+        
+
+        nums[j+1]=key
     
-    elif high-low==1:
-        return min(nums[low],nums[high]),max(nums[low],nums[high])
-    
 
-    mid=(high+low)//2
-
-    left_min,left_max=min_max(nums,low,mid)
-    right_min,right_max=min_max(nums,mid+1,high)
-
-    return min(left_min,right_min),max(left_max,right_max)
-
+    return nums
 
 
 
 nums=[1,8,9,6,3,5,0]
-print(min_max(nums,0,len(nums)-1))
+print(insertion_sort(nums))
