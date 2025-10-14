2891af6e9abe0e6f5b6ce5cff6ad69b4b467c251
Chandresh-spec <chandumoger819@gmail.com>
2025-10-14 21:41:25 +0530

    selection sort

--- sortingAlgo.py/selectionsort.py
+++ sortingAlgo.py/selectionsort.py
@@ -2,13 +2,13 @@ def selectionSort(arr):
     for i in range(len(arr)):
         mini=i
         for j in range(i+1,len(arr)):
-
-            if  arr[j]<= arr[mini]:
+            if arr[j]<  arr[mini]:
                 mini=j
-
+            
+        
+    
         arr[mini],arr[i]=arr[i],arr[mini]
     
-
 
     return arr
 
