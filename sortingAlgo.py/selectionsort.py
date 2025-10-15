6d927d57a62b0fa2b98e671b2caa17730c1c7bdf
Chandresh-spec <chandumoger819@gmail.com>
2025-10-15 22:24:51 +0530

    sloved selection sort agos

--- sortingAlgo.py/selectionsort.py
+++ sortingAlgo.py/selectionsort.py
@@ -1,19 +1,17 @@
-def selectionSort(arr):
-    for i in range(len(arr)):
+def selection_sort(nums):
+    for i in range(len(nums)):
         mini=i
-        for j in range(i+1,len(arr)):
-            if arr[j]<  arr[mini]:
+        for j in range(i+1,len(nums)):
+
+            if nums[j]<= nums[mini]:
                 mini=j
+
             
-        
-    
-        arr[mini],arr[i]=arr[i],arr[mini]
+        nums[mini],nums[i]=nums[i],nums[mini]
     
 
-    return arr
+    return nums
 
 
-
-
-arr=[1,8,9,6,7,5,3,2]
-print(selectionSort(arr))
+nums=[1,8,9,6,3,5,0]
+print(selection_sort(nums))
