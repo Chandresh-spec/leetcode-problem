40b01dbc9300a7fdc05ae5b34090a4b8eaa64b90
Chandresh-spec <chandumoger819@gmail.com>
2025-10-27 17:19:05 +0530

    sloved moves to zero problem

--- Arrays/revision/revision.py
+++ Arrays/revision/revision.py
@@ -93,6 +93,26 @@ def rotatearray(nums,k):
 nums=[-1,-100,3,99]
 k=2
 print(rotatearray(nums,k))
+
+
+
+
+from typing import List
+def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i=0
+        for j in range(len(nums)):
+
+            if nums[j]!=0:
+                nums[i],nums[j]=nums[j],nums[i]
+                i+=1
+            
+        return nums
+
+       
+
         
 
 
