ab149014a86e93ffe64c00541486f0fc7740d68b
Chandresh-spec <chandumoger819@gmail.com>
2025-10-28 19:30:34 +0530

    revised continuous subarray pattern

--- Arrays/patterns/prefix sum and difference array/partcise.py
+++ Arrays/patterns/prefix sum and difference array/partcise.py
@@ -1,22 +1,49 @@
 
-def subarraySum(nums,k) -> int:
-        prefix_sum=0
-        hashmap={0:1}
-        count=0
+# def subarraySum(nums,k) -> int:
+#         prefix_sum=0
+#         hashmap={0:1}
+#         count=0
 
-        for i in range(len(nums)):
-            prefix_sum+=nums[i]
+#         for i in range(len(nums)):
+#             prefix_sum+=nums[i]
 
 
-            if prefix_sum - k in hashmap:
-                count+=hashmap[prefix_sum-k]
+#             if prefix_sum - k in hashmap:
+#                 count+=hashmap[prefix_sum-k]
             
-            hashmap[prefix_sum]=hashmap.get(prefix_sum,0)+1
+#             hashmap[prefix_sum]=hashmap.get(prefix_sum,0)+1
         
 
-        return count
+#         return count
 
 
-nums=[1,1,1]
-k=2
-print(subarraySum(nums,k))
+# nums=[1,1,1]
+# k=2
+# print(subarraySum(nums,k))
+
+
+
+# continous subarray
+
+from typing import List
+def checkSubarraySum(nums: List[int], k: int) -> bool:
+        hashmap={0:-1}
+        sum_of=0
+
+        for i in range(len(nums)):
+            sum_of+=nums[i]
+            val=sum_of%k
+
+            if val in hashmap:
+                if i-hashmap[val] >= 1 :
+                    return True
+                
+            else:
+                 hashmap[val]=i
+        
+        return False
+
+
+nums=[5,0,0,0]
+k=3
+print(checkSubarraySum(nums,k))
