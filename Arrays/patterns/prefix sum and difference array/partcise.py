a2df09b690ee909a5d52cdf4442b545601454381
Chandresh-spec <chandumoger819@gmail.com>
2025-10-28 19:41:49 +0530

    revised subarray divisible by k problem

--- Arrays/patterns/prefix sum and difference array/partcise.py
+++ Arrays/patterns/prefix sum and difference array/partcise.py
@@ -26,24 +26,48 @@
 # continous subarray
 
 from typing import List
-def checkSubarraySum(nums: List[int], k: int) -> bool:
-        hashmap={0:-1}
-        sum_of=0
+# def checkSubarraySum(nums: List[int], k: int) -> bool:
+        # hashmap={0:-1}
+        # sum_of=0
+# 
+        # for i in range(len(nums)):
+            # sum_of+=nums[i]
+            # val=sum_of%k
+# 
+            # if val in hashmap:
+                # if i-hashmap[val] >= 1 :
+                    # return True
+                # 
+            # else:
+                #  hashmap[val]=i
+        # 
+        # return False
+# 
+# 
+# nums=[5,0,0,0]
+# k=3
+# print(checkSubarraySum(nums,k))
 
-        for i in range(len(nums)):
-            sum_of+=nums[i]
+
+
+
+
+
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        sum_of=0
+        count=0
+        hashmap={0:1}
+        for num in nums:
+            sum_of+=num
+
             val=sum_of%k
 
             if val in hashmap:
-                if i-hashmap[val] >= 1 :
-                    return True
-                
-            else:
-                 hashmap[val]=i
+                count+=hashmap[val]
+            
+            hashmap[val]=hashmap.get(val,0)+1
         
-        return False
 
-
-nums=[5,0,0,0]
-k=3
-print(checkSubarraySum(nums,k))
+        return count
+       
