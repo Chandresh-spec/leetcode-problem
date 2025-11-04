1c6c62d329f9cdab97f0edbf5c4b400d2155d8d9
Chandresh-spec <chandumoger819@gmail.com>
2025-11-04 16:25:29 +0530

    basic of strings

--- strings/basicofStrings.py/theory.py
+++ strings/basicofStrings.py/theory.py
@@ -1,58 +1,70 @@
-# string is sequence of character enclosed in quotes
+# # string is sequence of character enclosed in quotes
 
 
-# single='chandresh'
-# db="moger"
-# trip='''moger'''
+# # single='chandresh'
+# # db="moger"
+# # trip='''moger'''
 
 
-# print(single)
-# for i in single:
-#     print(i)
+# # print(single)
+# # for i in single:
+# #     print(i)
 
 
-# 
-# 
-# name="chandresh"
-# 
-# print(name.endswith('sh'))
-# 
-# name="moge, moger "
-# 
-# l=name.split(',')
-# print(l)
-# 
-# 
-# s="madum"
-# print(s[::-1])
-# 
-# 
-# count=0
-# for num in s:
-    # if num in "aeiou":
-        # count+=1
-    # 
-# print(count)
-# 
-# 
-# lne=len(s)-1
-# n=len(s)//2
-# for i in range(n):
-    # if s[i]!=s[lne-i]:
-        # print(False)
-    # 
-# print(True)
+# # 
+# # 
+# # name="chandresh"
+# # 
+# # print(name.endswith('sh'))
+# # 
+# # name="moge, moger "
+# # 
+# # l=name.split(',')
+# # print(l)
+# # 
+# # 
+# # s="madum"
+# # print(s[::-1])
+# # 
+# # 
+# # count=0
+# # for num in s:
+#     # if num in "aeiou":
+#         # count+=1
+#     # 
+# # print(count)
+# # 
+# # 
+# # lne=len(s)-1
+# # n=len(s)//2
+# # for i in range(n):
+#     # if s[i]!=s[lne-i]:
+#         # print(False)
+#     # 
+# # print(True)
 
 
-s="aabbcde"
+# s="aabbcde"
 
-for char in s :
-    if s.count(char)==1:
-        print(char)
-        break
+# for char in s :
+#     if s.count(char)==1:
+#         print(char)
+#         break
 
 
-s='  cha  n d '
+# s='  cha  n d '
 
-n=s.replace(" ","")
-print(n)
+# n=s.replace(" ","")
+# print(n)a
+
+
+
+# s="my name is chandu"
+# for i in range(s):
+    # print()
+
+
+s = "the sky is blue"
+print(len(s))
+for i in s:
+    print(i,end="")
