b51b1b1ecd7aa42bed0657d61507c5ed2aca8717
Chandresh-spec <chandumoger819@gmail.com>
2025-11-15 21:01:59 +0530

    sloved painterproblem using binary search implementation

--- binary-serach/minmax and maxmin pattern/painterProblem.py
+++ binary-serach/minmax and maxmin pattern/painterProblem.py
@@ -1,36 +1,71 @@
-Painter's Partition
+# Painter's Partition
 
 
-0
+# 0
 
-100
-You are given A painters and an array C of N integers where C[i] denotes the length of the ith board. Each painter takes B units of time to paint 1 unit of board. You must assign boards to painters such that:
+# 100
+# You are given A painters and an array C of N integers where C[i] denotes the length of the ith board. Each painter takes B units of time to paint 1 unit of board. You must assign boards to painters such that:
 
-Each painter paints only contiguous segments of boards.
-No board can be split between painters.
-The goal is to minimize the time to paint all boards.
-
-
-Return the minimum time required to paint all boards modulo 10000003.
+# Each painter paints only contiguous segments of boards.
+# No board can be split between painters.
+# The goal is to minimize the time to paint all boards.
 
 
-Examples:
-Input: A = 2, B = 5, C = [1, 10]
+# Return the minimum time required to paint all boards modulo 10000003.
 
-Output: 50
 
-Explanation:
+# Examples:
+# Input: A = 2, B = 5, C = [1, 10]
 
-Painter 1 paints board 0 (length = 1), time = 5
-Painter 2 paints board 1 (length = 10), time = 50
-Max time = 50
-Return 50 % 10000003 = 50
-Input: A = 10, B = 1, C = [1, 8, 11, 3]
+# Output: 50
 
-Output: 11
+# Explanation:
 
-Explanation:
+# Painter 1 paints board 0 (length = 1), time = 5
+# Painter 2 paints board 1 (length = 10), time = 50
+# Max time = 50
+# Return 50 % 10000003 = 50
+# Input: A = 10, B = 1, C = [1, 8, 11, 3]
 
-Assign each board to a different painter
-Max time = max(1, 8, 11, 3) = 11
-Return 11 % 10000003 = 11
+# Output: 11
+
+# Explanation:
+
+# Assign each board to a different painter
+# Max time = max(1, 8, 11, 3) = 11
+# Return 11 % 10000003 = 11
+
+
+
+
+
+
+def countPainters(boards, time):
+    n = len(boards)  # size of array
+    painters = 1
+    boardsPainter = 0
+    for i in range(n):
+        if boardsPainter + boards[i] <= time:
+            # allocate board to current painter
+            boardsPainter += boards[i]
+        else:
+            # allocate board to next painter
+            painters += 1
+            boardsPainter = boards[i]
+    return painters
+
+
+def findLargestMinDistance(boards, k):
+    low = max(boards)
+    high = sum(boards)
+
+    for time in range(low, high+1):
+        if countPainters(boards, time) <= k:
+            return time
+    return low
+
+
+boards = [10, 20, 30, 40]
+k = 2
+ans = findLargestMinDistance(boards, k)
+print("The answer is:", ans)
