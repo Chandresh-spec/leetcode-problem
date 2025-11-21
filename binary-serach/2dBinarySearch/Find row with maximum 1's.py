1e276a3b4b7dbdd1b2ab8b423b854e0a1408273a
Chandresh-spec <chandumoger819@gmail.com>
2025-11-21 17:24:28 +0530

    solved find row with maximum 1s problem brute approach using 2d matrix traversal

---
+++ binary-serach/2dBinarySearch/Find row with maximum 1's.py
@@ -0,0 +1,48 @@
+# Find row with maximum 1's
+
+
+# 0
+
+# 100
+# Easy
+
+# Given a non-empty grid mat consisting of only 0s and 1s, where all the rows are sorted in ascending order, find the index of the row with the maximum number of ones.
+
+# If two rows have the same number of ones, consider the one with a smaller index. If no 1 exists in the matrix, return -1.
+
+
+# Examples:
+# Input : mat = [ [1, 1, 1], [0, 0, 1], [0, 0, 0] ]
+
+# Output: 0
+
+# Explanation: The row with the maximum number of ones is 0 (0 - indexed).
+
+# Input: mat = [ [0, 0], [0, 0] ]
+
+# Output: -1
+
+# Explanation: The matrix does not contain any 1. So, -1 is the answer.
+
+
+
+
+def Matrix(nums):
+    maxi=-1
+    n=len(nums[0])
+    m=len(nums)
+    for  i in range(n):
+        count=0
+        for j in range(m):
+            if nums[i][j]==1:
+                count+=1
+            
+        
+        maxi=max(maxi,count)
+    
+    return maxi
+
+
+mat = [ [0, 0, 1], [0, 1, 1], [0, 1, 1] ]
+
+print(Matrix(mat))
