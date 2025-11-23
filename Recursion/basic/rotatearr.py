1b9176f65fbd118697976ae5ff22fa16f5b1bf02
Chandresh-spec <chandumoger819@gmail.com>
2025-11-23 16:24:09 +0530

    sloved reverse an array problem using recusrion

---
+++ Recursion/basic/rotatearr.py
@@ -0,0 +1,14 @@
+def helper(arr,n,index):
+            if index>=n//2:
+                return arr
+            
+            arr[index],arr[n-index-1]=arr[n-index-1],arr[index]
+            return helper(arr,n,index+1)
+        
+       
+
+
+
+arr= [1,2,3,4,5]
+n=5
+print(helper(arr,n,0))
