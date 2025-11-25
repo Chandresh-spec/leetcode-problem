86721dc8b23c41a66829085f814f61b00e2deff3
Chandresh-spec <chandumoger819@gmail.com>
2025-11-25 23:21:33 +0530

    sloved book allocation problem

--- binary-serach/partice.py/parctise.py
+++ binary-serach/partice.py/parctise.py
@@ -1,44 +1,44 @@
 
-def aggresiveCow(nums,k):
+# def aggresiveCow(nums,k):
 
-      def check(nums,j,k):
-            count=1
-            cow1=nums[0]
-            for i in range(1,len(nums)):
+#       def check(nums,j,k):
+#             count=1
+#             cow1=nums[0]
+#             for i in range(1,len(nums)):
 
-                  if nums[i]-cow1>=j:
-                        count+=1
-                        cow1=nums[i]
+#                   if nums[i]-cow1>=j:
+#                         count+=1
+#                         cow1=nums[i]
             
-            return count
-            
-
+#             return count
             
 
+            
 
 
 
 
 
 
-      ub=max(nums)
-      lb=min(nums)
 
-      while lb <= ub :
-            mid=(ub+lb)//2
+#       ub=max(nums)
+#       lb=min(nums)
 
-
-            checkans=check(nums,mid,k)
+#       while lb <= ub :
+#             mid=(ub+lb)//2
 
 
-            if checkans < k:
-                  ub=mid-1
+#             checkans=check(nums,mid,k)
 
-            else:
-                  lb=mid+1
+
+#             if checkans < k:
+#                   ub=mid-1
+
+#             else:
+#                   lb=mid+1
       
 
-      return ub
+#       return ub
 
                   
 
@@ -48,6 +48,54 @@ def aggresiveCow(nums,k):
 
 
 
-nums = [10, 1, 2, 7, 5]
-nums.sort()
-print(aggresiveCow(nums,2))
+# nums = [10, 1, 2, 7, 5]
+# nums.sort()
+# print(aggresiveCow(nums,2))
+
+
+
+
+
+def BookAllocation(nums,k):
+
+
+      def check(nums,j,k):
+            count=1
+            sum_of=0
+            for i in range(len(nums)):
+               if sum_of+nums[i]<=j:
+                  sum_of+=nums[i]
+            
+               else:
+                  sum_of=nums[i]
+                  count+=1
+            
+            return count
+            
+    
+
+
+
+      lb=max(nums)
+      ub=sum(nums)
+
+      while lb <= ub :
+            mid=(ub+lb)//2
+
+            anscheck=check(nums,mid,k)
+
+
+            if anscheck > k:
+                 lb=mid+1
+            else:
+                ub=mid-1
+
+      return lb
+            
+      
+      
+
+
+k=2
+nums =  [12, 34, 67, 90]
+print(BookAllocation(nums,k))
