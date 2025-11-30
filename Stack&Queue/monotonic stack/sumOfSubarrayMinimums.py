c67172e4fc2857e2ed34b05a93a0c0c1e4360083
Chandresh-spec <chandumoger819@gmail.com>
2025-11-30 10:13:47 +0530

    sloved sumofsubarrayminimum problem using stack

--- Stack&Queue/monotonic stack/sumOfSubarrayMinimums.py
+++ Stack&Queue/monotonic stack/sumOfSubarrayMinimums.py
@@ -44,8 +44,73 @@ def sumSubarrayMins(arr):
                 
                 total+=current
         
-        return current
+        return total
     
 
-arr = [3,1,2,4]
-print(sumSubarrayMins(arr))
+# arr = [3,1,2,4]
+# print(sumSubarrayMins(arr))
+
+
+
+
+
+
+def  nse(nums):
+     stack=[]
+     n=len(nums)
+     arr=[0]*n
+
+     for i in range(n-1,-1,-1):
+            while stack and stack[-1]>= nums[i]:
+               stack.pop()
+            
+
+            arr[i]=stack[-1]if stack else n
+
+            stack.append(nums[i])
+        
+     return arr
+
+
+
+def pse(nums):
+    stack=[]
+    n=len(nums)
+    arr=[0]*n
+
+    for i in range(len(nums)):
+        while stack and stack[-1]>=nums[i]:
+              stack.pop()
+        
+     
+        arr[i]=stack[-1]if stack else -1
+
+        stack.append(nums[i])
+    
+    return arr
+
+
+
+
+def subarray(nums):
+    nse1=nse(nums)
+    pse1=pse(nums)
+    total=0
+
+    for i in range(len(nums)):
+         
+         left=i-pse1[i]
+
+         right=nse1[i]-i
+
+
+         freq=(left*right)*1
+
+         val=(freq*nums[i])
+
+        
+    return total
+                        
+            
+arr = [11,81,94,43,3]
+print(subarray(arr))
