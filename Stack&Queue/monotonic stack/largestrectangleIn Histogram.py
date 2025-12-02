76628152fdc2cac30f0a91d18d59bc5b389a1545
Chandresh-spec <chandumoger819@gmail.com>
2025-12-02 23:11:42 +0530

    sloved  largest rectangle in histogram problem

---
+++ Stack&Queue/monotonic stack/largestrectangleIn Histogram.py
@@ -0,0 +1,45 @@
+def nse(nums):
+    stack=[]
+    n=len(nums)
+    arr=[n]*n
+    for i in range(n-1,-1,-1):
+        while  stack and nums[stack[-1]]>nums[i]:
+            stack.pop()
+        
+        arr[i]=stack[-1] if stack else n
+
+
+        stack.append(i)
+    
+    return arr
+
+
+
+def pse(nums):
+    stack=[]
+    n=len(nums)
+    arr=[n]*n
+    for i in range(n):
+        while  stack and nums[stack[-1]]>nums[i]:
+            stack.pop()
+     
+        arr[i]=stack[-1] if stack else -1
+        stack.append(i)
+ 
+    return arr
+    
+
+
+
+
+def histogram(nums):
+    nse1=nse(nums)
+    pse1=pse(nums)
+    maxi=0
+    for i in range(len(nums)):
+        current=(nse1[i]-pse1[i]-1)*nums[i]
+        maxi=max(maxi,current)
+    return maxi
+
+nums=[2,4]
+print(histogram(nums))
