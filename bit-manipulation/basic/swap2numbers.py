e9309b01fa160be409b8a4b9d75d25477a59ffb5
Chandresh-spec <chandumoger819@gmail.com>
2025-12-10 11:40:32 +0530

    sloved set the rightmost unset bit problem

--- bit-manipulation/basic/swap2numbers.py
+++ bit-manipulation/basic/swap2numbers.py
@@ -59,7 +59,7 @@ while n >1:
 
     n=n>>2
 
-print(ans+1)
+# print(ans+1)
 
 
 
@@ -69,4 +69,13 @@ while n!=0:
     n=n&(n-1)
     count+=1
 
-print(count)
+# print(count)
+
+
+
+
+
+n=13
+
+n=n|(n+1)
+print(n)
