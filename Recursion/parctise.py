e1642652a9984b3267c14ae87fef43e13f844f36
Chandresh-spec <chandumoger819@gmail.com>
2025-12-17 12:24:37 +0530

    learned recursion

--- Recursion/parctise.py
+++ Recursion/parctise.py
@@ -1,16 +1,38 @@
-def natural_Number(n,total):
-    if n==0:
-        return total
-        
+# def helper(cnt):
+#     if cnt==4:
+#         return
     
-    natural_Number(n-1,total+n)
+
+#     print(cnt)
+#     helper(cnt+1)
+
+
+
+# cnt=0
+# helper(cnt)
 
 
 
 
-n=5
-print(natural_Number(n,0))
+def target_sum(nums,target):
+    ans=0
+    def f(count,index):
+        nonlocal ans
+        if index==len(nums):
+            if count==target:
+                ans+=1
+            
+            return
+        
+        f(count+nums[index],index+1)
+        f(count,index+1)
 
+    
+    f(0,0)
+    print(ans)
 
-
+nums=[4, 2, 10, 5, 1, 3]
+target=5
+target_sum(nums,target)
+    
 
