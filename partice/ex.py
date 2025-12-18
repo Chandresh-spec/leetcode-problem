61f5539fb557bbc9c7cb9ed5e57791bd6e956e7e
Chandresh-spec <chandumoger819@gmail.com>
2025-12-18 22:24:14 +0530

    sloved check pallidrme problem

--- partice/ex.py
+++ partice/ex.py
@@ -22,23 +22,90 @@
 
 
 # 
-n=13
-count=0
-while n!=0:
-    n=n&(n-1)
-    count+=1
-    
-    
+# n=13
+# count=0
+# while n!=0:
+    # n=n&(n-1)
+    # count+=1
+    # 
+    # 
 
 
-print(count)
-n=13
-if n==1 or n==2 or n==3:
-    print("Prime")
+# print(count)
+# n=13
+# if n==1 or n==2 or n==3:
+    # print("Prime")
 
-if n%2==0 or n %3==0:
-    print("Not pRIME")
-else:
-    print("prime")
+# if n%2==0 or n %3==0:
+    # print("Not pRIME")
+# else:
+    # print("prime")
+
+
+
+
+# def validpallindrome(s):
+    # char=""
+    # for i in s:
+        # if i.isalnum():
+        # 
+            # char+=i.lower()
+# 
+    # print(char) 
+    # lb=0
+    # ub=len(char)-1
+# 
+    # while lb <= ub :
+        # if char[lb]!=char[ub]:
+            # return False
+        # 
+# 
+        # lb+=1
+        # ub-=1
+        # 
+    # 
+# 
+    # return True
+
+
+
+
+
+
+
+
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.strip()
+        left,right=0,len(s)-1
+
+
+        while left< right:
+            while not s[left].isalnum() and left < right:
+                left+=1
+            
+
+            while not s[right].isalnum() and left<right:
+                right-=1
+            
+
+
+            if s[left].lower()!=s[right].lower():
+                return False
+
+            right-=1
+            left+=1
+
+        
+        return True
+
+        
+
+
+
+
+
 
 
