2953a395439c20daab1dc29d3fa536673a3680c5
Chandresh-spec <chandumoger819@gmail.com>
2026-01-05 20:10:07 +0530

    revision

--- binary-serach/partice.py/parctise.py
+++ binary-serach/partice.py/parctise.py
@@ -1,37 +1,86 @@
+# 
+# def shipWithinDays(weights,days):
+# 
+# 
+# 
+        # def check(j,weights,days):
+            # sum_of=0
+            # count=1
+# 
+            # for i in range(len(weights)):
+                # if weights[i]+sum_of <=j:
+                    # sum_of+=weights[i]
+                # else:
+                    # count+=1
+                    # sum_of=weights[i]
+            # 
+            # return count==days
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+        # lb,ub=min(weights),sum(weights)
+        # for i in range(lb,ub):
+            # if check(i,weights,days):
+                # return i
+        # 
+        # return -1
+# 
+# 
+# 
+# 
+# weights = [1,2,3,1,1]
+# days = 4
+# print(shipWithinDays(weights,days))
 
-def shipWithinDays(weights,days):
 
 
 
-        def check(j,weights,days):
-            sum_of=0
-            count=1
-
-            for i in range(len(weights)):
-                if weights[i]+sum_of <=j:
-                    sum_of+=weights[i]
-                else:
-                    count+=1
-                    sum_of=weights[i]
-            
-            return count==days
+def aggressive_cow(nums,k):
 
 
+    def check(j,k,nums):
+        count=1
 
+        cow=nums[0]
 
-
-
-
-        lb,ub=min(weights),sum(weights)
-        for i in range(lb,ub):
-            if check(i,weights,days):
-                return i
+        for i in range(len(nums)):
+             if nums[i]-cow>=j:
+                  count+=1
+                  cow=nums[i]
         
-        return -1
+
+        return count
+
+     
 
 
 
+    lb=1
+    ub=max(nums)
 
-weights = [1,2,3,1,1]
-days = 4
-print(shipWithinDays(weights,days))
+    while lb <= ub :
+            mid=(ub+lb)//2
+            val= check(mid,k,nums)
+            if val >=k:
+                 lb=mid+1
+            else:
+                 ub=mid-1
+        
+
+    return ub
+
+      
+         
+        
+    
+
+
+
+k=2
+nums =  [4, 2, 1, 3, 6]
+nums.sort()
+print(aggressive_cow(nums,k))
