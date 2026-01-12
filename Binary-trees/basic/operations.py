6d41ed9cffea6143d9ed42c8757afe4a70e4a806
Chandresh-spec <chandumoger819@gmail.com>
2026-01-12 16:36:04 +0530

    learned about deleting specific node

--- Binary-trees/basic/operations.py
+++ Binary-trees/basic/operations.py
@@ -42,8 +42,41 @@ def insertion(root,data):
             q.append(current.right)
 
 
+def delete_targeted_node(root,target):
+    q=deque([root])
+    parent_last=None
+    current=None
+    target_node=None
+
+    while q:
+        current=q.popleft()
+        if current.data==target:
+            target_node=current
 
 
+        if current.left:
+           q.append(current.left)
+           parent_last=current
+
+
+
+        if current.right:
+            q.append(current.right)
+            parent_last=current
+
+
+    if target_node is None:
+        return root
+    
+    target_node.data=current.data
+
+
+    if parent_last.left==current:
+        parent_last.left=None
+
+    if parent_last.right==current:
+        parent_last.right=None
+    
 
 
 
@@ -59,4 +92,7 @@ n1.right.left=Node(60)
 n1.right.right=Node(70)
 
 insertion(s1.root,80)
+
+delete_targeted_node(s1.root,30)
 preorder(s1.root)
+
