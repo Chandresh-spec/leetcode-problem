273d25730dddeea853a91eaaf7148cc04dba913d
Chandresh-spec <chandumoger819@gmail.com>
2026-01-12 12:17:12 +0530

    learned about preorder using stack

--- Binary-trees/basic/implimentationUsingList.py
+++ Binary-trees/basic/implimentationUsingList.py
@@ -12,3 +12,52 @@ def preorder(arr,index):
 
 arr=[10,20,30,40,50,60,70]
 print(preorder(arr,0))
+
+
+
+
+class Node:
+    def __init__(self,data):
+       self.data=data
+       self.left=None
+       self.right=None
+
+
+
+class Tree:
+    def __init__(self):
+        self.root=None
+
+    
+
+
+def traversal(root):
+    stack=[root]
+    while not stack:
+        ele=stack.pop()
+
+        print(ele.data)
+
+        if ele.right:
+            stack.append(ele.right)
+
+        if ele.left:
+            stack.append(ele.left)
+
+
+
+
+
+
+t1=Tree()
+n1=Node(10)
+t1.root=n1
+n1.left=Node(20)
+n1.right=Node(30)
+n1.left.left=Node(40)
+n1.left.right=Node(50)
+n1.right.left=Node(40)
+n1.right.right=Node(40)
+
+traversal(t1.root)
+
