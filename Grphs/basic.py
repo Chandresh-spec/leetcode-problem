1210afcfc899a2a5d1acf3c963b8414946eccb17
Chandresh-spec <chandumoger819@gmail.com>
2026-02-04 11:37:26 +0530

    revised recursion

---
+++ Grphs/basic.py
@@ -0,0 +1,23 @@
+# n=5
+
+# graph=[[0 for _ in range(n+1)] for _ in range(n+1)]
+# edges=int(input("enter the number of edges"))
+
+# for i in range(edges):
+#     u=int(input("enter the u value:"))
+#     v=int(input("enter the v value:"))
+    
+    
+#     graph[u][v]=1
+#     graph[v][u]=1
+
+
+
+# print(graph)
+
+
+n=5
+
+graph = [[] for _ in range(n + 1)]
+
+print(graph)
