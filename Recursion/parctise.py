9b580fee2afba1d29c5a43e4768bc25b1f6ca0ed
Chandresh-spec <chandumoger819@gmail.com>
2026-02-05 15:10:46 +0530

    revised combination sum2 problem

--- Recursion/parctise.py
+++ Recursion/parctise.py
@@ -1,20 +1,57 @@
-def subsequence(nums,k):
+# def subsequence(nums,k):
+
+#     def helper(index,count):
+#         if len(nums)==index:
+#             if count==k:
+#                 return 1
+#             return 0
+        
+
+#         l=helper(index+1,count+nums[index])
+#         r=helper(index+1,count)
+
+#         return l+r
+#     return helper(0,0)
+
+        
+
+# nums=[1,2,1]
+# k=2
+# print(subsequence(nums,k))
+
+
+
+
+def combinationSum(candidates,target):
     ans=[]
-    def helper(index,count):
-        if len(nums)==index:
-            if count==k:
-                return 1
-            return 0
+    def helper(index,ds,target):
+        if target==0:
+            ans.append(ds[:])
         
+        for i in range(index,len(candidates)):
 
-        l=helper(index+1,count+nums[index])
-        r=helper(index+1,count)
+            if i>index and candidates[i]==candidates[i-1]:
+                continue
 
-        return l+r
-    return helper(0,0)
-
+            if candidates[i]>target:
+                break
+            
+            ds.append(candidates[i])
+            helper(i+1,ds,target-candidates[i])
+            ds.pop()
         
+    
+    helper(0,[],target)
+    return ans
+        
+       
 
-nums=[1,2,1]
-k=2
-print(subsequence(nums,k))
+candidates = [2,5,2,1,2]
+target = 5
+candidates.sort()
+print(combinationSum(candidates,target))
+
+
+
+
+
