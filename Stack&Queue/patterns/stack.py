b75f98b54564ca8512691eb70312a01aa0b5e382
Chandresh-spec <chandumoger819@gmail.com>
2026-02-12 19:33:54 +0530

    revised next greater element2

--- Stack&Queue/patterns/stack.py
+++ Stack&Queue/patterns/stack.py
@@ -1,22 +1,55 @@
-class Solution:
-    def decodeString(self, s: str) -> str:
-        stack=[]
-        for char in s:
-            if char != "]":
-                stack.append(char)
+# class Solution:
+#     def decodeString(self, s: str) -> str:
+#         stack=[]
+#         for char in s:
+#             if char != "]":
+#                 stack.append(char)
             
-            else:
-                substr=""
-                while stack and  stack[-1].isalpha():
-                    ch=stack.pop()
+#             else:
+#                 substr=""
+#                 while stack and  stack[-1].isalpha():
+#                     ch=stack.pop()
 
-                    substr=ch+substr
-                stack.pop()
-                num=""
-                while stack and stack[-1].isdigit():
-                    num=stack.pop()+num
+#                     substr=ch+substr
+#                 stack.pop()
+#                 num=""
+#                 while stack and stack[-1].isdigit():
+#                     num=stack.pop()+num
                 
-                stack.append(substr*int(num))
+#                 stack.append(substr*int(num))
             
         
-        return "".join(stack)
+#         return "".join(stack)
+
+
+
+def  NextGreaterElementI(nums1,nums2):
+    stack=[]
+    hashmap={}
+    for i in range(len(nums2)-1,-1,-1):
+          
+        while stack and stack[-1]<nums2[i]:
+            stack.pop()
+        
+        if stack:
+            hashmap[nums2[i]]=stack[-1]
+        
+        stack.append(nums2[i])
+    
+
+    arr=[-1]*len(nums1)
+    for i in range(len(nums1)):
+        if nums1[i] in hashmap:
+            arr[i]=hashmap[nums1[i]]
+        
+
+    return arr
+    
+
+        
+
+
+nums1 = [4,1,2]
+nums2 = [1,3,4,2]
+
+print(NextGreaterElementI(nums1,nums2))
