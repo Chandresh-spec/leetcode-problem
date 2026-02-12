66649fad2c5fe2576352aabe780906fbad900342
Chandresh-spec <chandumoger819@gmail.com>
2026-02-12 20:14:34 +0530

    revised nextgreater2 element

--- Stack&Queue/patterns/stack.py
+++ Stack&Queue/patterns/stack.py
@@ -23,33 +23,66 @@
 
 
 
-def  NextGreaterElementI(nums1,nums2):
-    stack=[]
-    hashmap={}
-    for i in range(len(nums2)-1,-1,-1):
+# def  NextGreaterElementI(nums1,nums2):
+#     stack=[]
+#     hashmap={}
+#     for i in range(len(nums2)-1,-1,-1):
           
-        while stack and stack[-1]<nums2[i]:
-            stack.pop()
+#         while stack and stack[-1]<nums2[i]:
+#             stack.pop()
         
-        if stack:
-            hashmap[nums2[i]]=stack[-1]
+#         if stack:
+#             hashmap[nums2[i]]=stack[-1]
         
-        stack.append(nums2[i])
+#         stack.append(nums2[i])
     
 
-    arr=[-1]*len(nums1)
-    for i in range(len(nums1)):
-        if nums1[i] in hashmap:
-            arr[i]=hashmap[nums1[i]]
+#     arr=[-1]*len(nums1)
+#     for i in range(len(nums1)):
+#         if nums1[i] in hashmap:
+#             arr[i]=hashmap[nums1[i]]
         
 
-    return arr
+#     return arr
     
 
         
 
 
-nums1 = [4,1,2]
-nums2 = [1,3,4,2]
+# nums1 = [4,1,2]
+# nums2 = [1,3,4,2]
 
-print(NextGreaterElementI(nums1,nums2))
+# print(NextGreaterElementI(nums1,nums2))
+
+
+
+
+
+def nextGreaterELement2(nums):
+        n=len(nums)
+        arr=[-1]*len(nums)
+        stack=[]
+
+        # for i in range(len(nums)):
+        #     for j in range(i,n*2):
+        #         j=j%n
+        #         if nums[j]>nums[i]:
+        #              arr[i]=nums[j]
+        #              break
+        
+        # return arr
+        for i in range(n*2-1,-1,-1):
+                while stack and nums[i%n]>=stack[-1]:
+                        stack.pop()
+                
+                if i < n:
+                        if stack:
+                                arr[i]=stack[-1]
+                
+                stack.append(nums[i%n])
+        
+        return arr
+                
+nums =[1,2,1]
+
+print(nextGreaterELement2(nums))      
