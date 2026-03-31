95284a4b65a0c2a1aa3a77e3ff47f43b78029e75
Chandresh-spec <chandumoger819@gmail.com>
2026-03-31 19:35:38 +0530

    sloved validate binary tree problem

--- binary-serach/partice.py/parctise.py
+++ binary-serach/partice.py/parctise.py
@@ -1,100 +1,120 @@
-
-# def findPages(nums, m):
-#         def check(i,nums,m):
-#             total=0
-#             count=0
-#             for j in range(len(nums)):
-#                 if total+nums[j]<=i:
-#                     total+=nums[j]
-                
-#                 else:
-#                     total=nums[j]
-#                     count+=1
-                
-            
-#             if total!=0:
-#                  count+=1
-            
-#             return count
-
-
-        
-#         lb=max(nums)
-#         ub=sum(nums)
-
-#         while lb <=ub :
-#             mid=(ub+lb)//2
-
-#             total=check(mid,nums,m)
-
-#             if total <=m:
-#                  ub=mid-1
-#             else:
-#                  lb=mid+1
-                
-            
-#         return lb
-
-
-
+class Node:
+    def __init__(self,data):
+        self.left=None
+        self.right=None
+        self.data=data
     
-# nums = [25, 46, 28, 49, 24]
-# m=4
-# print(findPages(nums,m))
 
 
+def preorder(root):
+    if root is None:
+        return
+    print(root.data)
+    preorder(root.left)
+    preorder(root.right)
 
+def search(root,target):
 
-# def rowWithMax1s(mat):
-#     index=-1
-#     maxi=0
-#     n,m=len(mat),len(mat[0])
-#     for i in range(n):
-#         count=0
-#         for j in range(m):
-#             if mat[i][j]==1:
-#                 count+=1
+    if root is None:
+        return
+    while root :
+        if target==root.data:
+            return True
+        if target < root.data:
+            root=root.left
+        else:
+            root=root.right
         
-#         if count>maxi:
-#             index=i
-#             maxi=count
+    return False
+
+def insertion(root,data):
+    if root is None:
+        return Node(data)
+    
+    while root:
+        if data<root.data:
+            if root.left:
+              root=root.left
+            else:
+                root.left=Node(data)
+                break
+                
+        else:
+            if root.right:
+               root=root.right
+            else:
+                root.right=Node(data)
+                break
+
+def RecursiveInsertion(root,data):
+    if root is None:
+        return Node(data)
+    
+    if data < root.data:
+        root.left=RecursiveInsertion(root.left,data)
+    else:
+        root.right=RecursiveInsertion(root.right,data)
+    
+
+    return root
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def isValidBST(root):
+        # q=deque()
+
+        # q.append((root,float('-inf'),float('inf')))
+
+        # while q:
+        #     temp,mini,maxi=q.popleft()
+
+
+        #     if mini >= temp.val or  temp.val>=maxi:
+        #         return False
+            
+        #     if temp.left:
+        #         q.append((temp.left,mini,temp.val))
+            
+        #     if temp.right:
+        #         q.append((temp.right,temp.val,maxi))
+    
+        # return True
+
+
+        def helper(root,mini,maxi):
+            if root is None:
+                return True
+            
+            if mini >= root.val or  root.val>=maxi:
+                return False
+            
+
+            return helper(root.left,mini,root.val) and helper(root.right,root.val,maxi)
         
-#     return index
 
+        return helper(root,float('-inf'),float('inf'))
 
-
-
-
-# mat = [ [0, 0, 1], [0, 1, 1], [0, 1, 1] ]
-# print(rowWithMax1s(mat))
-
-
-
-
-def findPeakGrid(mat):
-        m=len(mat[0])
-        n=len(mat)
-        for i in range(n):
-            for j in range(m):
-                left=mat[i][j-1] if j>0 else -1
-
-                right=mat[i][j+1] if j < m-1 else -1
-
-                top=mat[i-1][j] if i>0 else -1
-
-                bottom=mat[i+1][j] if  i < n-1 else -1
-
-
-                current=mat[i][j]
-
-                if current>left and current>right and current>top and current>bottom:
-                    return [i,j]
         
-        return False
 
 
 
 
-mat = [[10,20,15],[21,30,14],[7,16,32]]
 
-print(findPeakGrid(mat))
+n1=Node(10)
+n1.left=Node(5)
+n1.left.left=Node(4)
+n1.left.right=Node(6)
+n1.right=Node(15)
+n1.right.left=Node(11)
+n1.right.right=Node(30)
+
+print(search(n1,85))
+insertion(n1,3)
+RecursiveInsertion(n1,7)
+preorder(n1)
