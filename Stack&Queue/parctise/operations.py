c92a163cae37c5610577b10a14b8b55392ccb475
Chandresh-spec <chandumoger819@gmail.com>
2026-04-30 10:13:59 +0530

    stack operations

---
+++ Stack&Queue/parctise/operations.py
@@ -0,0 +1,42 @@
+class stack:
+    def __init__(self):
+        self.stack=[]
+
+    
+    def __str__(self):
+        pass
+    
+
+    def is_empty(self):
+        if len(self.stack)==0:
+            return True
+        return False
+    
+    def peek(self):
+        if self.is_empty():
+            return "stack is empty"
+        return self.stack[-1]
+    
+
+    def push(self,value):
+        self.stack.append(value)
+        return f"{value} is appended,{self.stack}"
+    
+    def pop(self):
+        if self.is_empty():
+            return "stack is empty"
+        
+        value=self.stack.pop()
+        return f"{value} is popped form stack"
+    
+
+
+
+s1=stack()
+print(s1.is_empty())
+s1.push(10)
+s1.push(20)
+s1.push(30)
+print(s1.stack)
+s1.pop()
+print(s1.stack)
