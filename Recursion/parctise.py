1efbeb7b918b99e397886d36c766f8fae26d065b
Chandresh-spec <chandumoger819@gmail.com>
2026-07-12 17:09:51 +0530

    revised recusrion

--- Recursion/parctise.py
+++ Recursion/parctise.py
@@ -1,375 +1,282 @@
-# def subsequence(nums,k):
 
+
+
+
+
+
+
+# def subsequence(arr,k):
+
+#     ans=[]
+#     ds=[]
 #     def helper(index,count):
-#         if len(nums)==index:
+#         if index==len(arr):
+#             if count==k:
+#                 ans.append(ds[:])
+            
+#             return
+        
+
+#         ds.append(arr[index])
+#         helper(index+1,count+arr[index])
+#         ds.pop()
+#         helper(index+1,count)
+    
+
+    
+
+
+#     def helper1(index,count):
+#         if index==len(arr):
+#             if count==k:
+#                 ans.append(ds[:])
+#                 return True
+            
+#             return False
+        
+#         ds.append(arr[index])
+#         if (helper1(index+1,count+arr[index]))==True:
+#             return True
+#         ds.pop()
+#         if (helper1(index+1,count))==True:
+#             return True
+    
+
+#     def helper2(index,count):
+#         if index==len(arr):
 #             if count==k:
 #                 return 1
+        
 #             return 0
         
-
-#         l=helper(index+1,count+nums[index])
-#         r=helper(index+1,count)
-
+#         l=helper2(index+1,count+arr[index])
+#         r=helper2(index+1,count)
 #         return l+r
-#     return helper(0,0)
+    
 
+#     return helper2(0,0)
+    
+       
         
 
-# nums=[1,2,1]
-# k=2
+    
+    
+        
+
+
+
+
+
+
+
+# nums = [1, 2, 3, 4, 5] 
+# k = 8
 # print(subsequence(nums,k))
 
 
 
+# def combinationSum2(candidates,target):
 
-# def combinationSum(candidates,target):
-#     ans=[]
-#     def helper(index,ds,target):
-#         if target==0:
-#             ans.append(ds[:])
-        
-#         for i in range(index,len(candidates)):
+        # ans=set()
+        # ds=[]
+        # def helper(index,target):
+        #     if index==len(candidates):
+        #         if target==0:
+        #             ds.sort()
+        #             ans.add(tuple(ds[:]))
 
-#             if i>index and candidates[i]==candidates[i-1]:
-#                 continue
-
-#             if candidates[i]>target:
-#                 break
+        #         return
             
-#             ds.append(candidates[i])
-#             helper(i+1,ds,target-candidates[i])
-#             ds.pop()
-        
-    
-#     helper(0,[],target)
-#     return ans
-        
+        #     ds.append(candidates[index])
+        #     helper(index+1,target-candidates[index])
+        #     ds.pop()
+        #     helper(index+1,target)
        
+        # helper(0,target)
+        # final_ans=[list(item)for item in ans]
+        # return final_ans
 
-# candidates = [2,5,2,1,2]
-# target = 5
-# candidates.sort()
-# print(combinationSum(candidates,target))
 
 
-
-
-
-
-# def merge(arr,left,mid,right):
-#     left_index=left
-#     right_index=mid+1
-#     nums=[]
-
-#     while left_index<=mid and right_index<=right:
-#         if arr[left_index]<=arr[right_index]:
-#             nums.append(arr[left_index])
-#             left_index+=1
-        
-#         else:
-#             nums.append(arr[right_index])
-#             right_index+=1
-        
-    
-#     nums.extend(arr[left_index:])
-#     nums.extend(arr[right_index:])
-
-
-#     for i in range(left, right + 1):
-#             arr[i] = nums[i-left]
-
-
-
-
-
-# def mergesort(arr,left,right):
-#     if left>=right:
-#         return
-    
-#     mid=(right+left)//2
-
-#     mergesort(arr,left,mid)
-#     mergesort(arr,mid+1,right)
-#     return merge(arr,left,mid,right)
-
-
-
-
-# arr=[7, 4, 1, 5, 3]
-# print(mergesort(arr,0,len(arr)-1))
-# print(arr)
-
-
-
-
-# def partition(arr,low,high):
-#     pivot=arr[low]
-#     i=low
-#     j=high
-
-#     while i <j:
-#         while  arr[i]<=pivot and i<high:
-#             i+=1
-        
-#         while arr[j]>=pivot and j>low:
-#             j-=1
-        
-
-#         if i < j :
-#             arr[i],arr[j]=arr[j],arr[i]
-        
-    
-#     arr[j],arr[low]=arr[low],arr[j]
-
-#     return j
-
-
-
-
-
-# def quicksort(arr,low,high):
-#     if (low <high):
-#         pi=partition(arr,low,high)
-
-#         quicksort(arr,low,pi-1)
-#         quicksort(arr,pi+1,high)
-
-
-
-# arr= [7, 4, 1, 5, 3]
-
-# quicksort(arr,0,len(arr)-1)
-# print(arr)
-
-
-
-# def combination_sum(nums,target):
-#     ans=[]
-#     def helper(index,ds,target):
-#         if index >=len(nums):
-#             if target==0:
-#                 ans.append(ds[:])   
-#             return
-        
-#         if target>= nums[index]:
-#             ds.append(nums[index])
-#             helper(index,ds,target-nums[index])
-#             ds.pop()
-#         helper(index+1,ds,target)
-        
-    
-
-
-#     helper(0,[],target)
-#     return ans
-
-
-
-# nums=[2,3,6,7]
-# target=7
-# print(combination_sum(nums,target))
-
-
-
-
-
-
-# def combination2(nums,target):
-#     ans=[]
-#     def helper(index,ds,target):
-        
-#         if index>=len(nums):
-#             if target==0:
-#                 ans.append(ds[:])
-            
-#             return
-        
-
-#         if target>=nums[index]:
-#             ds.append(nums[index])
-#             helper(index+1,ds,target-nums[index])
-#             ds.pop()
-#         helper(index+1,ds,target)
-
-
-    
-#     helper(0,[],target)
-#     st=set()
-#     for i in ans:
-#         i.sort()
-#         st.add(tuple(i))
-    
-    
-
-#     return [ list(item) for item in st]
-    
-    
-
-# nums=[10,1,2,7,6,1,5]
-# target=8
-# print(combination2(nums,target))
-
-
-
-
-
-
-
-
-
-
-
-
-# def combinationSum2(candidates, target) :
 #         ans=[]
-#         def helper(index,target,ds):
+#         ds=[]
+#         def helper(index,target):
 #             if target==0:
 #                 ans.append(ds[:])
-
-#             if index>=len(candidates):
 #                 return
             
-#             for i in range(index,len(candidates)):
-#                 if candidates[i]>target:
-#                     break
 
-                
-#                 if i >index and candidates[i-1]==candidates[i]:
+#             for i in range(index,len(candidates)):
+#                 if i>index and candidates[i]==candidates[i-1]:
+                    
 #                     continue
 
+#                 if target<candidates[i]:
+#                     break
+
 #                 ds.append(candidates[i])
-#                 helper(i+1,target-candidates[i],ds)
+#                 helper(i+1,target-candidates[i])
 #                 ds.pop()
         
-#         helper(0,target,[])
+#         helper(0,target)
 #         return ans
-    
 
 
-# candidates = [10,1,2,7,6,1,5]
-# target = 8
+
+
+
+# candidates = [1,2,2,2,5]
+# target = 5
 # candidates.sort()
 # print(combinationSum2(candidates,target))
 
 
 
 
+
+
 # def combinationSum3(k,n):
 #         ans=[]
-#         def helper(index,ds,n):
+#         ds=[]
+#         def helper(index,count,k,n):
+
 #             if len(ds)==k:
-#                 if n==0:
-#                     ans.append(ds[:])
-                
-#                 return 
+#                  if count==n:
+#                       ans.append(ds[:])
+#                  return
             
+
 #             for i in range(index,10):
 
-#                 ds.append(i+1)
-#                 helper(i+1,ds,n-i)
+#                 ds.append(i)
+#                 helper(i+1,count+i,k,n)
 #                 ds.pop()
         
-
-
-#         helper(0,[],n)
+#         helper(1,0,k,n)
 #         return ans
+        
+        
 
 
 # k=3
 # n=7
+# print(combinationSum3(k,n))
 
 
 
 
-
-# def nqueen(n,ans):
-
-#     def safe(board,row,col,n):
-#         j=col
-#         for i in range(j-1,-1,-1):
-#             if board[row][i]=='Q':
-#                 return False
-        
-        
-#         #upperdiagnal
-#         i=row
-#         j=col
-#         while i>=0 and j >=0:
-#             if board[i][j]=='Q':
-#                 return False
-#             i-=1
-#             j-=1
-        
-
-#         #lowerdiagnal
-#         i=row
-#         j=col
-#         while i>=0 and i <= n and j >=0 and j <=n:
-#             if board[i][j]=='Q':
-#                 return False
-            
-#             i+=1
-#             j-=1
-        
-
-#         return True
-         
 
     
+    
+# def ispallindrome(index,i,s):
+#     while i>index:
+#         if s[i]!=s[index]:
+#             return False
+#         i-=1
+#         index+=1
         
+#     return True
 
-#     def helper(col,board):
-#         if col==n:
-#             ans.append([''.join(row) for row in board])
+
+
+
+
+
+# def partition(s: str):
+    
+#     ans=[]
+#     ds=[]
+#     def helper(index):
+#         if index==len(s):
+#             ans.append(ds[:])
 #             return
-#         for i in range(n):
-#             if safe(board,i,col,n-1):
-#                 board[i][col]='Q'
-#                 helper(col+1,board)
-#                 board[i][col]='.'
+        
+#         for i  in range(index,len(s)):
             
-#     board=[['.' for i in range(n)]for i in range(n)]
-#     helper(0,board)
+#             if  ispallindrome(index,i,s)==False:
+#                 continue
+            
+#             ds.append(s[index:i+1])
+#             helper(i+1)
+#             ds.pop()
+#     helper(0)
 #     return ans
-    
+        
+        
 
-
-
-# n=5
-# ans=[]
-# print(nqueen(n,ans))
+# s = "baa"
+# print(partition(s))
 
 
 
 
-def rateMaze(n,grid):
-    ans=[]
-    def helper(row,col,cur,visited):
-        if col==n-1 and row==n-1:
-            ans.append(cur)
-            return
+def exist(board,word):
 
-        if row < 0 or col <0 or row>n-1 or col>n-1 or visited[row][col] or grid[row][col]==0:
+    def helper(row,col,board,word,index,visited,m,n):
+
+        if index==len(word):
+            return True
+        
+        if col<0 or col>n-1 or row<0 or row>m-1 or visited[row][col]==True or board[row][col]!=word[index]:
             return False
         
         visited[row][col]=True
+        
 
-        helper(row-1,col,cur+'U',visited)
-        helper(row+1,col,cur+'D',visited)
-        helper(row,col+1,cur+'R',visited)
-        helper(row,col-1,cur+'L',visited)
-
+        if   (helper(row,col+1,board,word,index+1,visited,m,n)or
+                helper(row,col-1,board,word,index+1,visited,m,n)or
+                helper(row+1,col,board,word,index+1,visited,m,n)or
+                helper(row-1,col,board,word,index+1,visited,m,n))==True:
+            return True
+            
+        
 
         visited[row][col]=False
 
+        
+            
+            
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    m=len(board)
+    n=len(board[0])
+    visited=[[False for _ in range(n)]for _ in range(m)]
+    print(visited)
+    for i in range(m):
+        for j in range(n):
+            if board[i][j]==word[0]:
+                if(helper(i,j,board,word,0,visited,m,n))==True:
+                    return True
     
-    visited=[[False for _ in range(n)] for _ in range(n)]
-    helper(0,0,'',visited)
-    return ans
+    return False
+
+
+    
+
+board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+word = "ABCB"
+
+print(exist(board,word))
 
 
 
 
-n = 4
-grid = [ [1, 0, 0, 0] , [1, 1, 0, 1], [1, 1, 0, 0], [0, 1, 1, 1] ]
-print(rateMaze(n,grid))
+
+
+
+     
+       
+
+
+        
--- matrix/BASIC.py
+++ matrix/BASIC.py
@@ -18,107 +18,199 @@
 
 # # tarversal :- row major and colum major method
 
-# #ROW MAJOR
+# # #ROW MAJOR
 
-# m=len(mat)
-# n=len(mat[0])
-
-
-# for i in range(m):
-#     for j in range(n):
-#         print(mat[i][j],end=" ")
+# # m=len(mat)
+# # n=len(mat[0])
 
 
+# # for i in range(m):
+# #     for j in range(n):
+# #         print(mat[i][j],end=" ")
 
-# #COLUMN MAJOR METHOD
 
-# print()
-# for i in range(n):
-#     for j in range(m):
-#         print(mat[j][i],end=" ")
+
+# # #COLUMN MAJOR METHOD
+
+# # print()
+# # for i in range(n):
+# #     for j in range(m):
+# #         print(mat[j][i],end=" ")
 
 
     
 
 
-# nums=[[1,3,4],
-#      [4,8,9],
-#      [7,6,3]]
-# n=len(nums[0])
-# m=len(nums)
+# # nums=[[1,3,4],
+# #      [4,8,9],
+# #      [7,6,3]]
+# # n=len(nums[0])
+# # m=len(nums)
 
 
-# for  r in range(m):
-#     for c in range(n):
-#         print(nums[r][c],end=" ")
+# # for  r in range(m):
+# #     for c in range(n):
+# #         print(nums[r][c],end=" ")
     
-#     print()
+# #     print()
 
 
-# print("_"*4)
+# # print("_"*4)
 
-# for  c in range(n):
-#     for r in range(3):
-#         print(nums[r][c],end=" ")
+# # for  c in range(n):
+# #     for r in range(3):
+# #         print(nums[r][c],end=" ")
     
-#     print()
+# #     print()
 
 
 
 
 
 
-def setZeroes(matrix) -> None:
+# def setZeroes(matrix) -> None:
 
-    n=len(matrix[0])
-    m=len(matrix)
-    cols=matrix[0][0]
-    # c=[0]*n
-    # r=[0]*m
+#     n=len(matrix[0])
+#     m=len(matrix)
+#     cols=matrix[0][0]
+#     # c=[0]*n
+#     # r=[0]*m
 
-    # for i in range(m):
-    #     for j in range(n):
-    #         if matrix[i][j]==0:
-    #             c[j]=1
-    #             r[i]=1
-    
-
-    # for i in range(m):
-    #     for j in range(n):
-    #         if c[j]==1 or r[i]==1:
-    #             matrix[i][j]=0
+#     # for i in range(m):
+#     #     for j in range(n):
+#     #         if matrix[i][j]==0:
+#     #             c[j]=1
+#     #             r[i]=1
     
 
-    # return matrix
-    for i in range(m):
+#     # for i in range(m):
+#     #     for j in range(n):
+#     #         if c[j]==1 or r[i]==1:
+#     #             matrix[i][j]=0
+    
+
+#     # return matrix
+#     for i in range(m):
+#         for j in range(n):
+#             if  matrix[i][j]==0:
+#               if j ==0:
+#                 cols=0
+#               else:
+#                 matrix[0][j]=0
+#               matrix[i][0]=0
+    
+#     for i in range(1,m):
+#         for j in range(1,n):
+#             if matrix[0][j]==0 or matrix[i][0]==0:
+#                 matrix[i][j]=0
+    
+#     for i in range(1,n):
+#        if cols==0 or 
+       
+    
+#     # for c in range(m):
+#     #     if matrix[0][0]==0:
+#     #         for i in range(m):
+#     #             matrix[i][0]==0
+        
+
+#     for i in matrix:
+#         print(i,end=" ")
+#         print()
+    
+    
+
+
+
+# matrix = matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+# print(setZeroes(matrix))
+        
+
+
+# def RotateImage(matrix):
+   
+#     n=len(matrix[0])
+#     m=len(matrix)
+#     # arr=[[0 for _ in range(n)]for _ in range(n)]
+#     # for i in range(m):
+#     #     for j in range(n):
+#     #         arr[j][n-i-1]=matrix[i][j]
+    
+
+#     # return arr
+
+#     for i in range(n):
+#         for j in range(n):
+#             matrix[j][i],matrix[i][j]=matrix[i][j],matrix[j][i]
+        
+
+#     for i in matrix:
+#             print(i,end=" ")
+#             print()
+
+
+# matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# print(RotateImage(matrix))
+
+
+
+
+def setmatrix(matrix):
+        n=len(matrix[0])
+        m=len(matrix)
+
+        first_row=False
+        first_column=False  
         for j in range(n):
-            if  matrix[i][j]==0:
-              if j ==0:
-                cols=0
-              else:
+
+            if matrix[0][j]==0:
+                first_row=True
+                break
+        
+        for i in range(m):
+            if matrix[i][0]==0:
+                first_column=True
+                break
+        
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][j]==0:
+                    matrix[0][j]=0
+                    matrix[i][0]=0
+        
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+            
+
+        if first_row:
+            for j in range(n):
                 matrix[0][j]=0
-              matrix[i][0]=0
-    
-    for i in range(1,m):
-        for j in range(1,n):
-            if matrix[0][j]==0 or matrix[i][0]==0:
-                matrix[i][j]=0
-    
-    # for c in range(m):
-    #     if matrix[0][0]==0:
-    #         for i in range(m):
-    #             matrix[i][0]==0
         
 
-    for i in matrix:
-        print(i,end=" ")
-        print()
-    
-    
-
-
-
-matrix = matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
-print(setZeroes(matrix))
+        if first_column:
+            for i in range(m):
+                matrix[i][0]=0
         
 
+
+        return matrix
+
+              
+    
+  
+        for i in range(m):
+            for j in range(n):
+                print(matrix[i][j],end=" ")
+            print()
+
+
+
+
+
+matrix=[[1,1,1,1],[1,0,1,1],[0,1,1,1],[1,1,0,1]]
+print(setmatrix(matrix))
+                    
